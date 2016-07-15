fffbf0bd2105e022eea3b1ede17ec38b07cb0dc5
Arthur Carcano <arthur.carcano@ens.fr>
2016-07-15 17:42:56 +0900

    Replace compile flag by cmd line flags

--- Makefile
+++ Makefile
@@ -3,14 +3,8 @@ CC=clang
 CFLAGS=-Wall -O2 -Wpedantic -Wextra -Wno-gnu -std=gnu11
 LDLIBS=
 
-all: new legacy
-
-
-new:
-	$(CC) $(CFLAGS) $(LDLIBS) *.c -o $(NAME)2
-
-legacy:
-	$(CC) $(CFLAGS) $(LDLIBS) -DLEGACY *.c -o $(NAME)
+tps: *.o
+	$(CC) $(CFLAGS) $(LDLIBS) *.c -o $(NAME)
 	
 clean:
 	rm -f *.o $(NAME) *~
--- tps.c
+++ tps.c
@@ -1,49 +1,63 @@
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 
 #include "instance.h"
 #include "core.h"
 
 
-// Printing functions
-// ------------------
-
-
 int main(int argc, char * argv[]) {
+	bool DEBUG = false;
+	bool LEGACY = false;
 
-	FILE * f = stdin;
-	if(argc > 1) { 
-		f = fopen(argv[1],"r");
+	int c;
+	while ((c = getopt(argc , argv, "dl")) != -1) {
+    switch (c) {
+			case 'd':
+				DEBUG = true;
+				break;
+			case 'l':
+				LEGACY = true;
+				break;
+		}
 	}
+
+	//file given on cmd line, defaul to stdin
+	FILE * f = stdin;
+	if(argc > optind) {
+		f = fopen(argv[optind],"r");
+	}
+
 	size_t n, m;
 	fscanf(f," %zd %zd ",&n,&m);
+
 	Instance input = {
-		.height = n, 
+		.height = n,
 		.width = m,
 		.agents = calloc(n,sizeof(AJ_Entry)),
 		.jobs = calloc(m,sizeof(AJ_Entry)),
 		.matrix = malloc(n*sizeof(M_Entry*)),
 	};
+	//store the matrix in a contiguous region of memory
 	M_Entry * contiguous = calloc(n*m,sizeof(M_Entry));
 	for(size_t i = 0; i < n; i++) {
 		input.matrix[i] = contiguous + m*i;
 	}
+
 	uint64_t agents_sum = 0;
 	for(size_t i = 0; i < n; i++) {
-#ifdef LEGACY 
-		fscanf(f," %ld ",&input.agents[i].discr);
-#else
-		fscanf(f," %ld:%ld ",&input.agents[i].tag,&input.agents[i].discr);
-#endif
+		if(LEGACY)
+			fscanf(f," %ld ",&input.agents[i].discr);
+		else
+			fscanf(f," %ld:%ld ",&input.agents[i].tag,&input.agents[i].discr);
 		agents_sum += input.agents[i].discr;
 	}
 	uint64_t jobs_sum = 0;
 	for(size_t i = 0; i < m; i++) {
-#ifdef LEGACY 
-		fscanf(f," %ld ",&input.jobs[i].discr);
-#else
-		fscanf(f," %ld:%ld ",&input.jobs[i].tag,&input.jobs[i].discr);
-#endif
+		if(LEGACY)
+			fscanf(f," %ld ",&input.jobs[i].discr);
+		else
+			fscanf(f," %ld:%ld ",&input.jobs[i].tag,&input.jobs[i].discr);
 		jobs_sum += input.jobs[i].discr;
 	}
 	if (jobs_sum != agents_sum) {
@@ -58,14 +72,24 @@ int main(int argc, char * argv[]) {
 			input.matrix[i][j].val = tmp;
 		}
 	}
-	print_inst(&input);
-	prelim(&input);
-	print_inst(&input);
-	while(!done(&input)) {
+	if(DEBUG) {
+		printf("Input:\n");
 		print_inst(&input);
+		printf("-----------\n");
+	}
+	prelim(&input);
+	if(DEBUG) {
+		printf("After preliminary work:\n");
+		print_inst(&input);
+		printf("-----------\n");
+	}
+	while(!done(&input)) {
+		if(DEBUG)
+			print_inst(&input);
 		step1(&input);
 	}
-	print_inst(&input);
+	if(DEBUG)
+		print_inst(&input);
 	uint64_t cost = 0;
 	for (size_t i = 0; i < input.height; i++) {
 		for (size_t j = 0; j < input.width; j++) {
@@ -73,13 +97,13 @@ int main(int argc, char * argv[]) {
 		}
 	}
 	printf("Minimum cost: %ld\n",cost);
-#ifndef LEGACY
-	for (size_t i = 0; i < input.height; i++) {
-		for (size_t j = 0; j < input.width; j++) {
-			if (input.matrix[i][j].quota != 0) {
-				printf("%ld %ld %ld\n", input.agents[i].tag, input.jobs[j].tag, input.matrix[i][j].quota);
+	if(!LEGACY) {
+		for (size_t i = 0; i < input.height; i++) {
+			for (size_t j = 0; j < input.width; j++) {
+				if (input.matrix[i][j].quota != 0) {
+					printf("%ld %ld %ld\n", input.agents[i].tag, input.jobs[j].tag, input.matrix[i][j].quota);
+				}
 			}
 		}
 	}
-#endif
 }
